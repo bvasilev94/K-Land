1192614a10d1db4816204a9e81dd03b856405cec
biservasilev <biservasilev94@gmail.com>
2023-07-04 18:03:44 +0300

    Adding footer and making dropdown menu for account

--- k-land-personal-project/src/app/app.module.ts
+++ k-land-personal-project/src/app/app.module.ts
@@ -3,13 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './core/header/header.component';
-import { FooterComponent } from './core/footer/footer.component';
+
 import { MainComponent } from './main/main.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, MainComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, MainComponent],
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
