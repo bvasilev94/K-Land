82266a66b6b8fad14e21f84da4802065793a11a6
biservasilev <biservasilev94@gmail.com>
2023-07-05 22:43:39 +0300

    Adding views for register and login

--- k-land-personal-project/src/app/auth/register/register.component.ts
+++ k-land-personal-project/src/app/auth/register/register.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
-
+  register(data: object) {
+    console.log(data);
+  }
 }
