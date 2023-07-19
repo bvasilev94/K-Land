0d9eac48a43a617497f6bde62494d688287c36c1
biservasilev <biservasilev94@gmail.com>
2023-07-19 23:11:58 +0300

    Adding update component and path to it

--- k-land-personal-project/src/app/feature/feature-routing.module.ts
+++ k-land-personal-project/src/app/feature/feature-routing.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddProductComponent } from './add-product/add-product.component';
 import { MyProductsSellerComponent } from './my-products-seller/my-products-seller.component';
+import { UpdateProductSellerComponent } from './update-product-seller/update-product-seller.component';
 
 const routes: Routes = [
   { path: 'add-product', component: AddProductComponent },
-  { path: 'my-products', component: MyProductsSellerComponent },
+  {
+    path: 'my-products',
+    component: MyProductsSellerComponent,
+  },
+  {
+    path: 'my-products/:id/edit',
+    component: UpdateProductSellerComponent,
+  }
 ];
 
 @NgModule({
--- k-land-personal-project/src/app/feature/feature.module.ts
+++ k-land-personal-project/src/app/feature/feature.module.ts
@@ -8,9 +8,10 @@ import { HomeComponent } from './home/home.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { FeatureRoutingModule } from './feature-routing.module';
 import { MyProductsSellerComponent } from './my-products-seller/my-products-seller.component';
+import { UpdateProductSellerComponent } from './update-product-seller/update-product-seller.component';
 
 @NgModule({
-  declarations: [HomeComponent, AddProductComponent, MyProductsSellerComponent],
+  declarations: [HomeComponent, AddProductComponent, MyProductsSellerComponent, UpdateProductSellerComponent],
   imports: [CommonModule, RouterModule, FeatureRoutingModule, FormsModule],
   exports: [HomeComponent, AddProductComponent],
 })
