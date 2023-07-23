941b6c1c5b0e819dd1528157cae05a75b0a55b8d
biservasilev <biservasilev94@gmail.com>
2023-07-23 16:45:33 +0300

    Implemeting suggestions on keyevent in search bar

--- k-land-personal-project/src/app/core/header/header.component.ts
+++ k-land-personal-project/src/app/core/header/header.component.ts
@@ -2,14 +2,50 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { UserService } from 'src/app/auth/user.service';
+import { ProductService } from 'src/app/feature/product.service';
+import { AddProductData } from 'src/app/types/Product';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
-  constructor(private userService: UserService, private router: Router) {}
+export class HeaderComponent implements OnInit {
+  allProducts: AddProductData[] | undefined;
+  mathchingProducts: AddProductData[] | undefined;
+
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((data) => {
+      this.allProducts = data;
+    });
+  }
+
+  searchProduct(query: KeyboardEvent) {
+    if (query) {
+      const element = query.target as HTMLInputElement;
+      console.log(element.value);
+      if (this.allProducts) {
+        this.mathchingProducts = this.allProducts.filter((product) =>
+          product.name
+            .toLocaleLowerCase()
+            .includes(element.value.toLocaleLowerCase())
+        );
+        if (this.mathchingProducts.length > 5) {
+          this.mathchingProducts.length = 5;
+        }
+      }
+    }
+  }
+
+  hideSearchSuggestion() {
+    this.mathchingProducts = undefined;
+  }
 
   get isLoggedIn(): boolean {
     return this.userService.isLogged();
