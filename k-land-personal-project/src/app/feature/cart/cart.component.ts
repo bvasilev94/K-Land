d032f42f78ed9de9824e1f4c988a557e3f00ffe3
biservasilev <biservasilev94@gmail.com>
2023-07-28 18:18:27 +0300

    Adding html and css and populating data from local cart to cart component

--- k-land-personal-project/src/app/feature/cart/cart.component.ts
+++ k-land-personal-project/src/app/feature/cart/cart.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AddProductData } from 'src/app/types/Product';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cartItems: AddProductData[] | undefined
 
+  constructor() {}
+
+  ngOnInit(): void {
+    let currCart = localStorage.getItem('cart');
+    if (currCart) {
+      this.cartItems = JSON.parse(currCart);
+    }
+  }
 }
